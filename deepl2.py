2a0195c3e9e6f844ad72c4014bf7d1e3fb3ad21b
M-H0127 <85206771+M-H0127@users.noreply.github.com>
2021-07-29 09:35:05 +0900

    Add files via upload

--- deepl2.py
+++ deepl2.py
@@ -1,26 +1,26 @@
 import time
 from selenium import webdriver
 import chromedriver_binary
-import re
+from xml.sax.saxutils import unescape
 def deepl(text):
-    print(text)
+    print(text,"\n")
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    text=text.replace(".\r\n","あ")
-    text=text.replace("\r\n"," ")
-    text=text.replace("あ",".\r\n")
+    #options.add_argument('--headless')
+    text = ' '.join(text.splitlines())
     print(text)
-    url="https://www.deepl.com/ja/translator#en/ja/"+text
+    url="https://www.deepl.com/translator"
     driver =webdriver.Chrome(options=options)
     driver.get(url)
+    insec="#dl_translator > div.lmt__text > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--source > div.lmt__textarea_container > div.lmt__inner_textarea_container > textarea"
+    driver.find_element_by_css_selector(insec).send_keys(text)
     selector="#target-dummydiv"
     while 1:
         Outputtext =  driver.find_element_by_css_selector(selector).get_attribute("innerHTML")
         if Outputtext != "\r\n" :
             break
         time.sleep(1)
-    Outputtext =  driver.find_element_by_css_selector(selector).get_attribute("innerHTML")
-    driver.quit
+    #driver.quit
     Outputtext=Outputtext.rstrip("\r\n")
+    Outputtext=unescape(Outputtext)
     return Outputtext
 
