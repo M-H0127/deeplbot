f1eade04121d0d3bf5b74239c76ddca522bb9d24
M-H0127 <85206771+M-H0127@users.noreply.github.com>
2021-07-29 17:11:24 +0900

    Add files via upload

--- deepl2.py
+++ deepl2.py
@@ -12,11 +12,15 @@ def deepl(text):
     insec="#dl_translator > div.lmt__text > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--source > div.lmt__textarea_container > div.lmt__inner_textarea_container > textarea"
     driver.find_element_by_css_selector(insec).send_keys(text)
     selector="#target-dummydiv"
+    i=1
     while 1:
         Outputtext =  driver.find_element_by_css_selector(selector).get_attribute("innerHTML")
         if Outputtext != "\r\n" :
             break
+        elif i>30:
+            Outputtext="翻訳不可能な言語です\r\n"
         time.sleep(1)
+        i+=1
     driver.quit
     Outputtext=Outputtext.rstrip("\r\n")
     Outputtext=unescape(Outputtext)
