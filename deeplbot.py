cf16340402a074c30024baaa558b90ea55e81d25
M-H0127 <85206771+M-H0127@users.noreply.github.com>
2021-07-29 23:20:05 +0900

    Add files via upload

--- deeplbot.py
+++ deeplbot.py
@@ -1,3 +1,4 @@
+import deepl2
 import discord
 import asyncio
 import time
@@ -12,40 +13,6 @@ async def on_message(message):
     dm=await author.create_dm()
     text=message.content
     if message.channel==dm:
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        text = ' '.join(text.splitlines())
-        url="https://www.deepl.com/ja/translator"
-        driver =webdriver.Chrome(options=options)
-        driver.get(url)
-        i=0
-        while 1:
-            try:
-                insec="#dl_translator > div.lmt__text > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--source > div.lmt__textarea_container > div.lmt__inner_textarea_container > textarea"
-            except selenium.common.exceptions.NoSuchElementException as e:
-                if i<10:
-                    await asyncio.sleep(0.5)
-                    i+=1
-                else:
-                    Outputtext="エラーが発生しました"
-                    await message.channel.send(Outputext)
-            else:
-                break
-
-        driver.find_element_by_css_selector(insec).send_keys(text)
-        selector="#target-dummydiv"
-        i=1
-        while 1:
-            Outputtext =  driver.find_element_by_css_selector(selector).get_attribute("innerHTML")
-            if Outputtext != "\r\n" :
-                break
-            elif i>30:
-                Outputtext="翻訳不可能な言語です\r\n"
-                break
-            await asyncio.sleep(1)
-            i+=1
-        driver.quit
-        Outputtext=Outputtext.rstrip("\r\n")
-        honyaku=unescape(Outputtext)
+        honyaku = deepl2.deepl(pyperclip.paste())
         await message.channel.send(honyaku)
 client.run(TOKEN)
