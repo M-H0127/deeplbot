1a641b245ecb3eac8041e3102cca1c0f21b37a6b
M-H0127 <85206771+M-H0127@users.noreply.github.com>
2021-08-31 20:17:34 +0900

    Update deeplbot.py

--- deeplbot.py
+++ deeplbot.py
@@ -30,7 +30,7 @@ def deepl(text,driver):
             break
     try:
         driver.find_element_by_css_selector(insec).send_keys(text)
-    except selenium.common.exceptions.WebDriverException as e:
+    except unknown error as e:
         outputtext="エラーが発生しました"
         return outputtext
     selector="#dl_translator > div.lmt__text > div.lmt__sides_container > div.lmt__side_container.lmt__side_container--target > div.lmt__textarea_container > div.lmt__inner_textarea_container > textarea"
